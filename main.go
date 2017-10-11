e8dff362199363f1df359e284bef9eb184990516
Gabor Takacs <gabor.takacs@bitrise.io>
2017-10-11 13:25:15 +0200

    Fix licenses

--- main.go
+++ main.go
@@ -74,9 +74,12 @@ func (configs ConfigsModel) validate() error {
 // EnsureAndroidLicences ...
 func EnsureAndroidLicences(androidHome string) error {
 	licenceMap := map[string]string{
-		"android-sdk-license":         "\n8933bad161af4178b1185d1a37fbf41ea5269c55",
-		"android-sdk-preview-license": "\n84831b9409646a918e30573bab4c9c91346d8abd",
-		"intel-android-extra-license": "\nd975f751698a77b662f1254ddbeed3901e976f5a",
+		"android-sdk-license":           "8933bad161af4178b1185d1a37fbf41ea5269c55\n\nd56f5187479451eabf01fb78af6dfcb131a6481e",
+		"android-googletv-license":      "\n601085b94cd77f0b54ff86406957099ebe79c4d6",
+		"android-sdk-preview-license":   "\n84831b9409646a918e30573bab4c9c91346d8abd",
+		"intel-android-extra-license":   "\nd975f751698a77b662f1254ddbeed3901e976f5a",
+		"google-gdk-license":            "\n33b6a2b64607f11b759f320ef9dff4ae5c47d97a",
+		"mips-android-sysimage-license": "\ne9acab5b5fbb560a72cfaecce8946896ff6aab9d",
 	}
 
 	licencesDir := filepath.Join(androidHome, "licenses")
