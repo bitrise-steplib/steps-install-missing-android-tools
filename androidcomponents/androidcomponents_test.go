05e1fe96d95436637676a5e8d863467029d0cb5f
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-07-03 09:22:09 +0200

    Update androidcomponents_test.go

--- androidcomponents/androidcomponents_test.go
+++ androidcomponents/androidcomponents_test.go
@@ -1,15 +1,30 @@
 package androidcomponents
 
 import (
+	"errors"
 	"io"
 	"os/exec"
 	"testing"
 
 	commandv2 "github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/env"
 	"github.com/bitrise-steplib/steps-install-missing-android-tools/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_GivenCommand_WhenFails_ThenReturnsExitError(t *testing.T) {
+	// TODO: androidcomponents.NewCommandError requires a command execution function to return an *exec.ExitError,
+	// when the command was successfully executed, but returned non-zero exit status.
+	// In go-utils/v2@v2.0.0-alpha.15 the command package was updated to return a new custom error.
+	// Upgrading to this or higher version breaks androidcomponents.NewCommandError.
+	// This test ensures that the used go-utils/v2/command package works well with androidcomponents.NewCommandError.
+	factory := commandv2.NewFactory(env.NewRepository())
+	cmd := factory.Create("bash", []string{"-c", "exit 1"}, nil)
+	err := cmd.Run()
+	var exitErr *exec.ExitError
+	require.True(t, errors.As(err, &exitErr))
+}
 
 func Test_GivenInstallerAndGradlePrintsToStderr_WhenScanDependencies_ThenErrorContainStderr(t *testing.T) {
 	// Given
